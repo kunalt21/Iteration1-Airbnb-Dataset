719e9ffab4ee21eabf4000193f775498b375ba5f
kunal tajane <kdtajane@gmail.com>
2025-10-04 21:10:00 +0530

    description added

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 
 def get_data():
+    """
+    read data and return pandas dataframe
+    """
+
     pwd = os.getcwd()
     listing_data = os.path.join(pwd, "data/raw/bkk_listings.csv")
     df = pd.read_csv(listing_data)
@@ -12,6 +16,10 @@ def get_data():
 
 
 def get_avg_neighbourhood_price(df):
+    """
+    Core solution, suggesting price recommendation based on average price of the neighbourhood
+    """
+
     df_filt = df.drop(df[df["price"].isna()].index)
     avg_price = (
         df_filt.groupby(by="neighbourhood", as_index=False)["price"].mean().round(2)
@@ -20,6 +28,10 @@ def get_avg_neighbourhood_price(df):
 
 
 def write_data():
+    """
+    write cooked data ready for consumption
+    """
+
     get_avg_neighbourhood_price(get_data()).to_csv(
         os.path.join(os.getcwd(), "data/cal/avg_price.csv")
     )
